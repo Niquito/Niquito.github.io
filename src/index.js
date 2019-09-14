ca783590b81617e3402814043914c9085c9ff052
H. Nicolás Caffarini <ncaffarini@gmail.com>
2019-09-14 17:09:59 -0300

    Update index.js

--- src/index.js
+++ src/index.js
@@ -1,12 +1,47 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './Components/App';
-import * as serviceWorker from './serviceWorker';
+import { render } from 'react-dom';
+import { makeStyles } from '@material-ui/core/styles';
+import Paper from '@material-ui/core/Paper';
+import Card from '@material-ui/core/Card';
+import Grid from '@material-ui/core/Grid';
+import WorkIcon from '@material-ui/icons/Work';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const useStyles = makeStyles(theme => ({
+  root: {
+    flexGrow: 1,
+  },
+  paper: {
+    padding: theme.spacing(3),
+    textAlign: 'left',
+    color: theme.palette.text.secondary,
+  },
+}));
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
+export default function Layout() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <Grid container spacing={3}>
+        <Grid item xs={12} sm={4}>
+          <Card className={classes.paper}>Resumen</Card>
+        </Grid>
+        <Grid item xs={12} sm={8}>
+          <Grid container spacing={3}>
+          	<Grid item xs={12}>
+	          <Paper className={classes.paper}><WorkIcon></WorkIcon>xs=12 sm=6</Paper>
+	        </Grid>
+	        <Grid item xs={12}>
+	          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+	        </Grid>
+	        <Grid item xs={12}>
+	          <Paper className={classes.paper}>xs=12 sm=6</Paper>
+	        </Grid>
+	      </Grid>
+        </Grid>
+      </Grid>
+    </div>
+  );
+}
+
+render (<Layout />, document.getElementById("root"))
